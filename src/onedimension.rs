3fa46422b010983cf73a5dd1dc4078dc0eb7769d
Jakub Konka <kubkon@jakubkonka.com>
2015-01-17 23:09:08 +0000

    Implement 1D Kalman filter and add a test case.

--- src/onedimension.rs
+++ src/onedimension.rs
@@ -1,14 +1,14 @@
 #[derive(Show)]
 pub struct KalmanFilter1D {
-    pub init_state: f64,
-    pub init_variance: f64,
+    pub state: f64,
+    pub variance: f64,
 }
 
 impl KalmanFilter1D {
     pub fn new(init_state: f64, init_variance: f64) -> KalmanFilter1D {
         KalmanFilter1D {
-            init_state: init_state,
-            init_variance: init_variance,
+            state: init_state,
+            variance: init_variance,
         }
     }
 
@@ -17,13 +17,18 @@ impl KalmanFilter1D {
                     system_param: f64,
                     system_variance: f64,
                     measurement_param: f64,
-                    measurement_variance: f64) -> f64 {
+                    measurement_variance: f64) {
         // 1. State estimate extrapolation
+        let state_estimate = system_param * self.state;
         // 2. Error variance extrapolation
+        let var_estimate = system_param * system_param * self.variance + system_variance;
         // 3. Kalman gain
+        let k = (var_estimate * measurement_param)
+                / (measurement_param * measurement_param * var_estimate + measurement_variance);
         // 4. State estimate update
+        self.state = state_estimate + k * (measurement - measurement_param * state_estimate);
         // 5. Error variance update
-        1.0
+        self.variance = (1.0 - k * measurement_param) * var_estimate;
     }
 }
 
--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,39 @@
+use KalmanFilter1D;
+use std::rand::thread_rng;
+use std::rand::distributions::{Normal, IndependentSample};
+
 #[test]
-fn it_works() {
+fn kalman_filter_1d() {
+    // This test case is based on Example 4.2-1
+    // from the book "Applied Optimal Estimation"
+    //
+    // System model:
+    // x(k+1) = x(k)
+    // Measurement model:
+    // z(k) = x(k) + v(k)
+    // where
+    // v(k) ~ N(0, r)
+    //
+    // Assumptions:
+    // x(0) = 0
+    // var(0) = 1
+    // r = 1
+    let state = 5.0f64;
+    let variance = 1.0f64;
+    let r = 1.5f64;
+    let normal = Normal::new(0.0, r);
+    let mut rng = thread_rng();
+
+    let mut kf = KalmanFilter1D::new(state, variance);
+    for i in range(1, 10u) {
+        let measurement = state + normal.ind_sample(&mut rng);
+        let denominator = 1.0 + variance / r * i as f64;
+        let exp_state = kf.state
+                        + ((variance / r) / denominator)
+                        * (measurement - kf.state);
+        let exp_variance = variance / denominator;
+        kf.estimate(measurement, 1.0, 0.0, 1.0, r);
+        assert_almost_eq!(kf.state, exp_state, 1e-6);
+        assert_almost_eq!(kf.variance, exp_variance, 1e-6);
+    }
 }
